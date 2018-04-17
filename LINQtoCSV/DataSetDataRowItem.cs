e6f6ef22ca19bd3efb832b42c8303b1fe6bd05a5
Martin Holden <martin.holden@skillsoft.com>
2018-04-17 15:57:37 +0100

    Missing class

---
+++ LINQtoCSV/DataSetDataRowItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQtoCSV
+{
+    internal class DataSetDataRowItem : List<DataRowItem>, IDataRow
+    {
+    }
+
+}
